1cc66604237e9d4f6e90dc53f05a149e2614919d
giro39 <127617431+giro39@users.noreply.github.com>
2024-06-15 18:16:03 +0200

    Add main - probably finished

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -13,25 +13,47 @@ const Home = () => {
 
     return (
         <div className={styles.container}>
-            <div className={styles.logoContainer}>
-                <img 
-                    src="../../public/bazujepl_logo_orange.png" 
-                    alt="Bazuje.it"
-                    className={styles.logo}
-                />
+            <div className={styles.navbar}>
+                <div className={styles.logoContainer}>
+                    <img 
+                        src="../../public/bazujepl_logo_orange.png" 
+                        alt="Bazuje.it"
+                        className={styles.logo}
+                    />
+                </div>
+                <div className={styles.inputContainer}>
+                    <input 
+                        type="text" 
+                        className={styles.inputText} 
+                        // placeholder="Wyszukaj uczelnię..."
+                    />
+                    <img 
+                        src={theme === 'dark' ? "../../public/moon_icon.png" : "../../public/sun_icon_orange.png"}
+                        alt="Change theme" 
+                        className={styles.themeIcon}
+                        onClick={handleModeChange}
+                    />
+                </div>
             </div>
-            <div className={styles.inputContainer}>
-                <input 
-                    type="text" 
-                    className={styles.inputText} 
-                    // placeholder="Wyszukaj uczelnię..."
-                />
-                <img 
-                    src={theme === 'dark' ? "../../public/moon_icon.png" : "../../public/sun_icon_orange.png"}
-                    alt="Change theme" 
-                    className={styles.themeIcon}
-                    onClick={handleModeChange}
-                />
+            <div className={styles.main}>
+                <div className={styles.textMain}>
+                    <p className={styles.largeText}>
+                        Wyszukaj swoją <br /> 
+                        przyszłą uczelnię na <br /> 
+                        podstawie <span className={styles.textGradient1}> Twojej </span> 
+                        wizji <br />
+                        <span className={styles.textGradient2}> siebie </span>
+                    </p>
+                </div>
+                <div className={styles.buttonMain}>
+                    <button className={styles.quizButton}>
+                        pozwól nam poznać Twoje preferencje
+                    </button>
+                </div>
+            </div>
+            <div className={styles.topMajors}>
+                <p className={styles.topMajorsText}>Najlepsze kierunki informatyczne:</p>
+
             </div>
         </div>
     );
