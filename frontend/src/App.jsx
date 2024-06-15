15b920cb8adde0f7527aba815c4f58181aec9367
giro39 <127617431+giro39@users.noreply.github.com>
2024-06-15 14:40:27 +0200

    Add styling vars and themes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,7 +23,7 @@ const RegisterAndLogout = () => {
 const App = () => {
     return (
         <BrowserRouter>
-            <main>
+            <main theme='dark'>
                 <Routes>
                     <Route 
                         path='/' 
