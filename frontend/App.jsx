b743c9160d52e57f886414757f02585f7f833e80
giro39 <127617431+giro39@users.noreply.github.com>
2024-06-15 11:56:06 +0200

    Frontend setup

---
+++ frontend/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+
+import './styles/App.scss';
+
+
+const App = () => {
+    return (
+        <p>filip</p>
+    )
+};
+
+export default App;
