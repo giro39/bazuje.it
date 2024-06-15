20cd5edb8f55caf91f3a70fb6d89ab9729a21f1d
giro39 <127617431+giro39@users.noreply.github.com>
2024-06-15 16:43:51 +0200

    Add Navbar and themes changing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 
-import ProtectedRoute from "./components/ProtectedRoute"
+import ProtectedRoute from "./components/ProtectedRoute";
+
+import { ThemeContext, initialThemeContext } from './contexts/ThemeContext';
 
 
 import '../styles/App.scss';
@@ -21,31 +23,35 @@ const RegisterAndLogout = () => {
 }
 
 const App = () => {
+    const [theme, setTheme] = useState(initialThemeContext);
+
     return (
         <BrowserRouter>
-            <main theme='dark'>
-                <Routes>
-                    <Route 
-                        path='/' 
-                        element={<Navigate to={'/home'} replace={true} />}
-                    />
-                    <Route 
-                        path='/home'
-                        element={
-                            // <ProtectedRoute>
-                                <Home />
-                            // </ProtectedRoute>
-                        } 
-                    />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/logout" element={<Logout />} />
-                    <Route path="/register" element={<RegisterAndLogout />} />
+            <main theme={theme}>
+                <ThemeContext.Provider value={{ theme, setTheme }}>
+                    <Routes>
+                        <Route 
+                            path='/' 
+                            element={<Navigate to={'/home'} replace={true} />}
+                        />
+                        <Route 
+                            path='/home'
+                            element={
+                                // <ProtectedRoute>
+                                    <Home />
+                                // </ProtectedRoute>
+                            } 
+                        />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/logout" element={<Logout />} />
+                        <Route path="/register" element={<RegisterAndLogout />} />
 
-                    <Route 
-                        path='*'
-                        element={<h1>404. Not found</h1>} 
-                    />
-                </Routes>
+                        <Route 
+                            path='*'
+                            element={<h1>404. Not found</h1>} 
+                        />
+                    </Routes>
+                </ThemeContext.Provider>
             </main>
         </BrowserRouter>
     )
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,40 @@
+import React, { useContext, useState } from 'react';
+
+import styles from '../../styles/pages/Home.module.scss';
+
+import { ThemeContext } from '../contexts/ThemeContext';
 
 const Home = () => {
-  return (
-    <p>abc</p>
-  );
+    const {theme, setTheme} = useContext(ThemeContext);
+
+    const handleModeChange = () => {
+        setTheme(prevState => (prevState === 'light' ? 'dark' : 'light'));
+    }
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.logoContainer}>
+                <img 
+                    src="../../public/bazujepl_logo_orange.png" 
+                    alt="Bazuje.it"
+                    className={styles.logo}
+                />
+            </div>
+            <div className={styles.inputContainer}>
+                <input 
+                    type="text" 
+                    className={styles.inputText} 
+                    // placeholder="Wyszukaj uczelnię..."
+                />
+                <img 
+                    src={theme === 'dark' ? "../../public/moon_icon.png" : "../../public/sun_icon_orange.png"}
+                    alt="Change theme" 
+                    className={styles.themeIcon}
+                    onClick={handleModeChange}
+                />
+            </div>
+        </div>
+    );
 };
 
 export default Home;
