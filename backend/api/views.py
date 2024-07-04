65df69c3fb4e38ab78da91b1846d3beb9f301ce2
FilipWitkowicz <filip.witkowicz1@gmail.com>
2024-07-04 16:34:10 +0200

    czyszczenie backendu

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,122 +1,93 @@
-from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .serializers import RodzajSerializer, MiastoSerializer, UczelniaSerializer, WydzialSerializer, KierunekSerializer, PrzedmiotSerializer, OpiniaPrzedmiotSerializer, OpiniaUczelniaSerializer, OpiniaKierunekSerializer
-from .models import Rodzaj, Miasto, Uczelnia, Wydzial, Kierunek, Przedmiot, OpiniaPrzedmiot, OpiniaUczelnia, OpiniaKierunek
-from django.contrib.auth.models import User
-import operator
-import json
-
-import random
-
-
-# @api_view(['GET'])
-# def getRoutes(request):
-#     routes = [
-#         {
-#             'Endpoint': '/',
-#             'method': 'GET',
-#             'body': None,
-#             'description': ''
-#         },
-#     ]
-#     return Response(routes)
-
-@api_view(['GET'])
-def getUczelnias(request):
-    notes = Uczelnia.objects.all()
-    serializer = UczelniaSerializer(notes, many=True)
-
-    
-    return Response(serializer.data)
-
-@api_view(['GET'])
-def getUczelnia(request, pk):
-    note = Uczelnia.objects.get(id=pk)
-    serializer = UczelniaSerializer(note, many=False)
-    return Response(serializer.data)
+from .serializers import (
+    BestKierunkiSerializer,
+    WynikQuizuSerializer,
+)
+from .models import (
+    Rodzaj,
+    Miasto,
+    Uczelnia,
+    Wydzial,
+    Kierunek,
+    Przedmiot,
+    OpiniaPrzedmiot,
+    OpiniaUczelnia,
+    OpiniaKierunek,
+    Kategorie,
+)
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def getBestKierunki(request):
-    n = Kierunek.objects.count()
-    kierunkiSrednie = {}
-    
-    for i in range(1, n + 1):
-        note = Kierunek.objects.get(id=i)
-        serializer = KierunekSerializer(note, many=False)
-        kierunek = serializer.data['nazwa']
-        id_kierunku = serializer.data['id']
-        uczelnia = note.wydzial.uczelnia
+    kierunki = Kierunek.objects.all()
 
-        opinie = OpiniaKierunek.objects.filter(kierunek=id_kierunku)
-        serializer2 = OpiniaKierunekSerializer(opinie, many=True)
-        sumaOcen = 0
-        for j in serializer2.data:
-            ocena = j['ocena']
-            sumaOcen += ocena
-        sredniaOcen = sumaOcen / len(serializer2.data)
-        kierunkiSrednie[kierunek] = [sredniaOcen, str(uczelnia)]
-    lista = []
-    for i in kierunkiSrednie:
-        podlista = []
-        podlista.append(i)
-        podlista.append(kierunkiSrednie[i][0])
-        podlista.append(kierunkiSrednie[i][1])
-        lista.append(podlista)
+    data = []
+    for kierunek in kierunki:
+        opinie = OpiniaKierunek.objects.filter(kierunek=kierunek.id)
 
-    
-    return Response(lista[:3])
+        sumaOcen = sum(opinia.ocena for opinia in opinie)
+        sredniaOcen = sumaOcen / len(opinie) if opinie else 0
 
-@api_view(['GET'])
+        data.append(
+            {
+                "kierunek": kierunek,
+                "uczelnia": kierunek.wydzial.uczelnia,
+                "sredniaOcen": sredniaOcen,
+            }
+        )
+        sorted_data = sorted(data, key=lambda x: x["sredniaOcen"], reverse=True)
+
+    serializer = BestKierunkiSerializer(sorted_data[:3], many=True)
+    return Response(serializer.data)
+
+
+kat1 = "BAZY DANYCH"
+kat2 = "WEB DEV"
+kat3 = "SIECI KOMPUTEROWE"
+
+
+@api_view(["GET"])
 def wynikQuizu(request):
-    n = Kierunek.objects.count()
-    kierunekOceny = {}
-    for i in range(1,n+1):
-        kierunek = Kierunek.objects.get(id=i)
-        uczelnia = kierunek.wydzial.uczelnia
-        print(uczelnia)
-        serializer = KierunekSerializer(kierunek, many=False)
-        nazwa_kierunek = serializer.data['nazwa']
-        id_kierunek = serializer.data['id']
-        przedmiot = Przedmiot.objects.filter(kierunek = id_kierunek)
-        serializer2 = PrzedmiotSerializer(przedmiot, many=True)
-        sumaOcenRangaPierwsza = 0
-        sumaOcenRangaDruga = 0
-        sumaOcenRangaTrzecia = 0
-        for j in serializer2.data:
-            nazwa_przedmiot = j['nazwa']
-            id_przedmiot = j['id']
-            opiniePrzedmiot = OpiniaPrzedmiot.objects.filter(przedmiot = id_przedmiot)
-            serializer3 = OpiniaPrzedmiotSerializer(opiniePrzedmiot, many=True)
+    kierunki = Kierunek.objects.all()
+    data = []
+    for kierunek in kierunki:
+        przedmioty = Przedmiot.objects.filter(kierunek=kierunek.id)
+        suma1, suma2, suma3 = 0, 0, 0
+        count1, count2, count3 = 0, 0, 0
+        for przedmiot in przedmioty:
 
-            for k in serializer3.data:
-                if j['kategoria'] == 1: # BAZY DANYCH
-                    sumaOcenRangaPierwsza += k['ocena']
-                elif j['kategoria'] == 3: # SIECI KOMPUTEROWE
+            if Kategorie(przedmiot.kategoria).label == kat1:
+                opinie = OpiniaPrzedmiot.objects.filter(przedmiot=przedmiot.id)
+                suma1 += sum(opinia.ocena for opinia in opinie)
+                count1 += len(opinie)
 
-                    sumaOcenRangaDruga += k['ocena']
-                elif j['kategoria'] == 2: # WEB DEV
+            if Kategorie(przedmiot.kategoria).label == kat2:
+                opinie = OpiniaPrzedmiot.objects.filter(przedmiot=przedmiot.id)
+                suma2 += sum(opinia.ocena for opinia in opinie)
+                count2 += len(opinie)
 
-                    sumaOcenRangaTrzecia += k['ocena']
-        sredniaOcenJeden = sumaOcenRangaPierwsza/len(serializer3.data)
-        sredniaOcenDwa = sumaOcenRangaDruga/len(serializer3.data)
-        sredniaOcenTrzy = sumaOcenRangaTrzecia/len(serializer3.data)
-        ocenaKoncowa = sredniaOcenJeden+0.7*sredniaOcenDwa+0.4*sredniaOcenTrzy
-        kierunekOceny[nazwa_kierunek] = [ocenaKoncowa, str(uczelnia)]
-    lista = []
-    for i in kierunekOceny:
-        podlista = []
-        podlista.append(kierunekOceny[i][0])
-        podlista.append(i)
-        
-        podlista.append(kierunekOceny[i][1])
-        lista.append(podlista)
+            if Kategorie(przedmiot.kategoria).label == kat3:
+                opinie = OpiniaPrzedmiot.objects.filter(przedmiot=przedmiot.id)
+                suma3 += sum(opinia.ocena for opinia in opinie)
+                count3 += len(opinie)
 
-    return Response((sorted(lista)[::-1])[:3])
+        wynikQuizu = 0
+        if count1 != 0:
+            wynikQuizu += 1 * suma1 / count1
+        if count2 != 0:
+            wynikQuizu += 0.7 * suma2 / count2
+        if count3 != 0:
+            wynikQuizu += 0.4 * suma3 / count3
 
-        # for j in serializer2.data:
-        #     if j['']
-        
+        data.append(
+            {
+                "kierunek": kierunek,
+                "uczelnia": kierunek.wydzial.uczelnia,
+                "wynikQuizu": wynikQuizu,
+            }
+        )
+        sorted_data = sorted(data, key=lambda x: x["wynikQuizu"], reverse=True)
 
-
+    serializer = WynikQuizuSerializer(sorted_data[:3], many=True)
+    return Response(serializer.data)
