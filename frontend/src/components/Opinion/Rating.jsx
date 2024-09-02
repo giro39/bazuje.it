a4ea6bcb7029f1a764467c3f688b714a3b8b4757
Fiflon <odzera.dotrofera@gmail.com>
2024-09-02 13:23:24 +0200

    Fixed the sending of the previous state

--- frontend/src/components/Opinion/Rating.jsx
+++ frontend/src/components/Opinion/Rating.jsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
-import styles from "../../styles/components/Opinion/Opinion.module.scss";
 import axios from "axios";
 import { jwtDecode } from "jwt-decode";
+import { useState } from "react";
+import styles from "../../styles/components/Opinion/Opinion.module.scss";
 
 import { FaArrowDown, FaArrowUp } from "react-icons/fa6";
 
@@ -11,32 +11,17 @@ const Rating = ({ rating, opinionId }) => {
     const [currentRating, setCurrentRating] = useState(rating);
     const [isVoted, setIsVoted] = useState(0);
 
-    function handleVote(vote) {
-        if (vote === isVoted) {
-            setCurrentRating(() => rating);
-            setIsVoted(0);
-        } else {
-            setIsVoted(vote);
-            vote === 1
-                ? setCurrentRating(() => rating + 1)
-                : setCurrentRating(() => rating - 1);
-        }
-        postVote();
-    }
-
-    0
-    const postVote = () => {
+    const postVote = (vote) => {
         const token = localStorage.getItem("access");
         if (token) {
             const decodedToken = jwtDecode(token);
             const userId = decodedToken.user_id;
-            console.log(userId, opinionId, isVoted);
+            console.log(userId, opinionId, vote);
             axios
                 .post(`${SERVER_URL}/api/vote/`, {
                     userId: userId,
                     opinionId: opinionId,
-                    grade: isVoted,
-
+                    grade: vote,
                 })
                 .then((response) => {
                     console.log(response.message);
@@ -47,6 +32,19 @@ const Rating = ({ rating, opinionId }) => {
         }
     };
 
+    function handleVote(vote) {
+        if (vote === isVoted) {
+            setCurrentRating(rating);
+            setIsVoted(0);
+            postVote(rating); // resetowanie głosu
+        } else {
+            setIsVoted(vote);
+            vote === 1
+                ? setCurrentRating(rating + 1)
+                : setCurrentRating(rating - 1);
+            postVote(vote);
+        }
+    }
 
     return (
         <div className={styles.ratingContainer}>
@@ -61,9 +59,7 @@ const Rating = ({ rating, opinionId }) => {
                 className={styles.ratingButton}
                 onClick={() => handleVote(-1)}
             >
-                <FaArrowDown
-                    className={`${isVoted === -1 && styles.active}`}
-                />
+                <FaArrowDown className={`${isVoted === -1 && styles.active}`} />
             </button>
         </div>
     );
